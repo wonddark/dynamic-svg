69363f1419245a3081367c4ba11076dc33b948da
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2023-02-20 22:18:58 -0500

    Main logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import "./App.css"
+import SVGComponent from "./SVGComponent";
 
 function App() {
+    const [component, setComponent] = useState("")
+  async function fetchImage() {
+     const res = await fetch("https://assets.gyfted.io/occassions/anniversary.svg")
+     const svg = await res.text();
+     setComponent(svg);
+  }
+  function loadImage(){
+    fetchImage().finally()
+  }
+  useEffect(loadImage, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="main-container">
+        <SVGComponent component={component}/>
     </div>
   );
 }
